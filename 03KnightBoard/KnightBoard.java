41abe628bd910f9e61e419c811111e7de4a86811
Kevin <kevinlula2014@gmail.com>
2017-02-17 15:33:28 -0500

    Finished

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,8 +1,8 @@
-public class KnightsTor{
+public class KnightBoard{
 
     public int [][] board;
 
-    public KnightsTor(int startingRows, int startingCols){
+    public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
 	for(int x = 0; x < startingRows; x++){
 	    for(int y = 0; y < startingCols; y++){
@@ -16,8 +16,8 @@ public class KnightsTor{
 	for(int x = 0; x < board.length; x++){
 	    for(int y = 0; y < board[0].length; y++){
 		if((board[x][y] / 10) == 0){
-		    res = res + " " + board[x][y];}
-		else{res = res + board[x][y];}
+		    res = res + " " + board[x][y] + " ";}
+		else{res = res + board[x][y] + " ";}
 	    }
 	    res = res + "\n";
 	}
@@ -25,41 +25,26 @@ public class KnightsTor{
     }
 
     public void solve(){
-	solve(0, 0, 1);
+	solveH(0, 0, 1);
     }
 
-    private boolean solveH(int row, int col, int level){
-	if(level == (board.length * board[0].length)){
-		return true;}
-	for(int x = 0; x < board.length; x++){
-	    for(int y = 0; y < board[0].length){
-		if(level == 1){
-		    board[x][y] = 1;
-		    }
-		return solveH(x + 2, y - 1, level + 1) ||
-		    solveH(x + 2, y + 1, level + 1) ||
-		    solveH(x - 2, y - 1, level + 1) ||
-		    solveH(x - 2, y + 1, level + 1) ||
-		    solveH(x + 1, y - 2, level + 1) ||
-		    solveH(x + 1, y + 2, level + 1) ||
-		    solveH(x - 1, y - 2, level + 1) ||
-		    solveH(x - 1, y + 2, level + 1);
-	    }
-
-
-    }
-}
 
 	private boolean solveH(int row, int col, int level){
-	if(level == (board.length * board[0].length)){
+	if(level == (board.length * board[0].length) + 1){
 	return true;}
 	for(int x = 0; x < board.length; x++){
 		for(int y = 0; y < board[0].length; y++){
+			if(level == 1){
+			board[x][y] = 1;
+			if(solveH(x, y, level + 1)){
+			return true;}
+			else{removeKnight(x, y);}
+			}
 			if(isValid(row, col, x, y)){
 			addKnight(x, y, level);
 			if(solveH(x, y, level + 1)){
 			return true;}
-			else{removeKnight(x, y, level);}
+			else{removeKnight(x, y);}
 			}
 			}
 			}
@@ -67,20 +52,24 @@ public class KnightsTor{
 
 	
     private boolean isValid(int row,int col,int x,int y){
-	if((row - 2 == x && col - 1 == y) || 
-	(row - 2 == x && col + 1 == y) ||
-	(row + 2 == x && col - 1 == y) ||
-	(row + 2 == x && col + 1 == y) ||
-	(row - 1 == x && col - 2 == y) ||
-	(row - 1 == x && col + 2 == y) ||
-	(row + 1 == x && col 
+	if((row - 2 == x && col - 1 == y && board[x][y] == 0) || 
+	(row - 2 == x && col + 1 == y && board[x][y] == 0) ||
+	(row + 2 == x && col - 1 == y && board[x][y] == 0) ||
+	(row + 2 == x && col + 1 == y && board[x][y] == 0) ||
+	(row - 1 == x && col - 2 == y && board[x][y] == 0) ||
+	(row - 1 == x && col + 2 == y && board[x][y] == 0) ||
+	(row + 1 == x && col - 2 == y && board[x][y] == 0) ||
+	(row + 1 == x && col + 2 == y && board[x][y] == 0)){
+	return true;}
+	else{return false;}
+}
 
     private void addKnight(int r, int c, int l){
-	board[r][c] == l;
+	board[r][c] = l;
     }
 
     private void removeKnight(int r, int c){
-	board[r][c] == 0;
+	board[r][c] = 0;
     }
 
 
@@ -90,3 +79,10 @@ public class KnightsTor{
 
     //1st Add Knight at starting position
     //1. If an L shape is presented in the loop then u add the knight of the next number 
+
+	public static void main(String[] args){
+	KnightBoard a = new KnightBoard(9,9);
+	a.solve();
+	System.out.println(a);
+}
+}
