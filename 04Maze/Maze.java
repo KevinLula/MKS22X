9d752d4582efb68f9cbb2f3c0f01f4d915f3db52
Kevin <kevinlula2014@gmail.com>
2017-02-27 15:34:10 -0500

    Started the Maze

---
+++ 04Maze/Maze.java
@@ -0,0 +1,44 @@
+public class Maze{
+
+    private char[][]maze;
+    private boolean animate;
+
+    public Maze(String filename){
+	animate = false;
+	//Scan Maze file
+    }
+
+    public void setAnimate(boolean b){
+	animate = b;
+    }
+
+    public void clearTerminal(){
+	System.out.println("\033[2J\033[1;1H");
+    }
+
+    public boolean solve(){
+	//FIND OUT WHERE START IS. Since the next three rows of code turn S into ' '
+	int rowStart = -1;
+	int colStart = -1;
+	maze[rowStart][colStart] = ' ';
+	return solve(rowStart, colStart);
+    }
+
+    private boolean solve(int row, int col){
+	if(animate){
+	    System.out.println("\033[2J\033[1;1H" + this);
+	    wait(20);
+	}
+
+	//COMPLETE
+	return false;
+    }
+
+    private void wait(int millis){
+	try{
+	    Thread.sleep(millis);
+	}
+	catch(InterruptedException e){
+	}
+    }
+}
