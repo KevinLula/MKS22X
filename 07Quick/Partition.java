6606e07cb70219c104a28a7f72f7b39077c4f662
KevinLula <kevinlula2014@gmail.com>
2017-03-15 23:58:50 -0400

    Works for partitioning whole array so could function with quickSelect

--- 07Quick/Partition.java
+++ 07Quick/Partition.java
@@ -1,23 +1,24 @@
+import java.util.Random;
 public class Partition{
-    public static int part(int [] data, int start, int end){
-	int[] temp = new int[end - start + 1];
-	int pivot = new Random().nextInt(end - start) + start;
-	int pivotValue = data[pivot];
-	int begin = 0;
-	int last = data.length - 1;
-	for(int x = start; start <= end; x++){
-	    if(x != pivot){
-		if(data[x] <= pivotValue){
-		    temp[begin] = data[x];
-		    begin += 1;
-		}
-		else{temp[last] = data[x];
-		    last -= 1;
-		}
+    public static void swap(int[]data, int a, int b){
+	int temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
+    }
+    
+    public static int part(int[] data, int start, int end){
+	int pivot = new Random().nextInt(data.length);
+	swap(data, pivot, end - 1);
+	int begin = start;
+	for(int i = start; i < end - 1; i++){
+	    if(data[i] < data[end-1]){
+		swap(data, begin, i);
+		begin+=1;	      
 	    }
 	}
-	temp[begin] = data[pivot];
-	return begin + start;
+	swap(data, begin, end-1);
+	return begin;		
     }
 }
+
 	
