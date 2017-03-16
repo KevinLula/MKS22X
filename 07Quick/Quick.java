6f90f3611f0ec7f299b4cf803cf1dd8416b6998c
KevinLula <kevinlula2014@gmail.com>
2017-03-16 00:00:13 -0400

    quickSelect functions by zoning out the area of which the kth smallest element is in

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,11 +1,40 @@
+import java.util.Random;
 public class Quick{
-
-    private int[] temp;
-
-    public static int quickSelect(int[] data, int k){
-	if(part(data, 0, data.length-1) == k){
-	    
-	}
-	else{quickSelect(data, k);}
+    public static void swap(int[]data, int a, int b){
+	int temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
     }
+    
+    public static int part(int[] data, int start, int end){
+	int pivot = new Random().nextInt(data.length);
+	swap(data, pivot, end - 1);
+	int begin = start;
+	for(int i = start; i < end - 1; i++){
+	    if(data[i] < data[end-1]){
+		swap(data, begin, i);
+		begin+=1;	      
+	    }
+	}
+	swap(data, begin, end-1);
+	return begin;		
+    }
+
+    public static int quickselect(int[]data, int k){
+	return quickH(data, k, 0, data.length);
+    }
+
+    public static int quickH(int[]data, int k, int start, int end){
+	int index = part(data, 0, data.length);
+	if (index == k){
+	    return data[k];
+	}
+	if(k > index){
+	    return quickH(data, k, start, index-1);
+	}
+	else{
+	    return quickH(data, k, index-1, end);
+	}
+    }
+}
 	
