c6c04e5521d5ae872223ab4b38306fa55bb0f371
KevinLula <kevinlula2014@gmail.com>
2017-03-16 02:47:45 -0400

    Only the latter half is failing at times and I dont know why

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -7,7 +7,7 @@ public class Quick{
 	data[end - 1] = data[pivot];
 	data[pivot] = temp;
 	int begin = start;
-	for(int x = start; x < end - 1; x++){
+	for(int x = start; x < end; x++){
 	    if(data[x] < data[end - 1]){
 		int temp2 = data[begin];
 		data[begin] = data[x];
@@ -25,7 +25,7 @@ public class Quick{
 	int index = part(data, 0, data.length - 1);
 	while(k != index){
 	    if(index > k){
-		index = part(data, 0, index);
+		index = part(data, 0, index + 1);
 	    }
 	    if(index < k){
 		index = part(data, index + 1, data.length - 1);
@@ -34,9 +34,17 @@ public class Quick{
 	return data[k];
     }
 
+
+    public static void main(String[]args){
+
+	int[] arr = new int[]{1, 3, 10, 11, 2, 5, 0, 8, 7};
+	//System.out.println(part(arr,0,9));
+	for(int i = 0; i < arr.length; i++){
+	    // System.out.print(arr[i]+" ");
+	    System.out.println(quickselect(arr,i));
+	}
+    }
 }
-
-
 
 
 	
