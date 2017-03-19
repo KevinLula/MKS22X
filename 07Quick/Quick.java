999f5550af85888062aa07afeca42c49f14d4ecb
KevinLula <kevinlula2014@gmail.com>
2017-03-19 18:27:11 -0400

    Quick sort isnt working for multiple of the same integer.

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -7,8 +7,8 @@ public class Quick{
 	data[end - 1] = data[pivot];
 	data[pivot] = temp;
 	int begin = start;
-	for(int x = start; x < end; x++){
-	    if(data[x] < data[end - 1]){
+	for(int x = start; x < end - 1; x++){
+	    if(data[x] <= data[end - 1]){
 		int temp2 = data[begin];
 		data[begin] = data[x];
 		data[x] = temp2;
@@ -60,23 +60,23 @@ public class Quick{
 	
 	    
 	public static void main(String[] args){
-	int[] a = {0, 4, 1, 5, 3, 2};
-	int[] b = {0, 4, 1, 5, 3, 2};;
-	int[] c = {0, 4, 1, 5, 3, 2};;
+	    int[] a = {0, 4, 1, 2, 5, 3, 2};
+	    int[] b = {0, 4, 1, 2, 5, 3, 2};
+	    int[] c = {0, 4, 1, 2, 5, 3, 2};
 	System.out.println(part(a, 0, a.length));
-	String s = "";
+		String s = "";
 	String t = "";
 	for(int x = 0; x < a.length; x++){
 	s += a[x];
 	}
 	System.out.println(s);
-	System.out.println(quickselect(b, 4));
+       	System.out.println(quickselect(b, 2));
 	for(int x = 0; x < b.length; x++){
 	t += b[x];
 	}
 	System.out.println(t);
 	System.out.println(quickSort(c));
-	}
+       	}
 
 }
 
