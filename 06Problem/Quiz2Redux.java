e8a22794d87b73987b5a55dab282e5049230cc09
Kevin <kevinlula2014@gmail.com>
2017-03-21 15:27:06 -0400

    Rename

---
+++ 06Problem/Quiz2Redux.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class Quiz2Redux{
+
+    public static ArrayList<String> combinations(String s){
+	ArrayList<String> words = new ArrayList<String>();
+	combinationH(s, words, "", 0);
+	return words;
+    }
+    private static void combinationH(String s, ArrayList<String> words, String a, int b){
+	int num = 0;
+	for(int x = 0; x < words.size(); x++){
+	    if(a.equals(words.get(x))){
+		num += 1;
+	    }
+	}
+	if(num == 0){
+	    words.add(a);
+	}
+	if(b < s.length()){
+	    combinationH(s, words, a + s.charAt(b), b + 1);
+	    combinationH(s, words, a, b + 1);
+	}
+    }
+}
