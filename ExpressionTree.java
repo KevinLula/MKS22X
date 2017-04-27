3e7e2aeadb7b2b5fbd8971f8a97dde8c990344af
Kevin <kevinlula2014@gmail.com>
2017-04-27 15:26:19 -0400

    Almost

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -66,4 +66,61 @@ public class ExpressionTree{
     }
 
     public double evaluate(){
- 
+ 	if(isValue()){
+	return getValue();
+}
+else{return apply(getOp(), getLeft().evaluate(), getRight().evaluate());}
+}
+
+    private double apply(Char op, Double b, Double a){
+	if (op == '+'){
+	    return a + b;}
+	if (op == '-'){
+	    return a - b;}
+	if(op == '*'){
+	    return a * b;}
+	if(op == '/'){
+	    return a / b;}
+	else{
+	    throw new IllegalArgumentException();}
+    }
+
+  public static void main(String[] args){
+    //ugly main sorry!
+    ExpressionTree a = new ExpressionTree(4.0);
+    ExpressionTree b = new ExpressionTree(2.0);
+
+    ExpressionTree c = new ExpressionTree('+',a,b);
+    System.out.println(c);
+    System.out.println(c.toStringPostfix());
+    System.out.println(c.toStringPrefix());
+    System.out.println(c.evaluate());
+
+
+    ExpressionTree d = new ExpressionTree('*',c,new ExpressionTree(3.5));
+    System.out.println(d);
+    System.out.println(d.toStringPostfix());
+    System.out.println(d.toStringPrefix());
+    System.out.println(d.evaluate());
+
+    ExpressionTree ex = new ExpressionTree('-',d,new ExpressionTree(1.0));
+    System.out.println(ex);
+    System.out.println(ex.toStringPostfix());
+    System.out.println(ex.toStringPrefix());
+    System.out.println(ex.evaluate());
+
+    ex = new ExpressionTree('+',new ExpressionTree(1.0),ex);
+    System.out.println(ex);
+    System.out.println(ex.toStringPostfix());
+    System.out.println(ex.toStringPrefix());
+    System.out.println(ex.evaluate());
+
+    ex = new ExpressionTree('/',ex,new ExpressionTree(2.0));
+    System.out.println(ex);
+    System.out.println(ex.toStringPostfix());
+    System.out.println(ex.toStringPrefix());
+    System.out.println(ex.evaluate());
+    
+    
+  }
+}
