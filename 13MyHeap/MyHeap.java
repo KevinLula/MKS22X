80f43ca8d91bd2bb73022ffba7f12b48eb2fdd41
Kevin <kevinlula2014@gmail.com>
2017-05-04 15:29:14 -0400

    Wrote pushUp and pushDown but might make into while loops

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -20,19 +20,57 @@ public class MyHeap{
     }
 
     public void add(String s){
+	array.add(s);
+	pushUp();
     }
 
     public String remove(){
+	return array.remove();
+	pushDown();
     }
 
     public String peek(){
+	return array.get(1);
     }
 
     private void pushUp(){
-	//if the value is smaller than the next top value then you stop.
+	pushUpHelp(array.size() - 1)
     }
 
+private void pushUpHelp(int x){
+	String str = array.get(x);
+	if(x > 1){
+	if(str.compareTo(array.get(x/2)) >= 1){
+	array.set(x) = array.get(x/2);
+	array.set(x/2) = str;
+	pushUpHelp(x/2);
+	}
+}
+}
+
     private void pushDown(){
-	if(this.compareTo(
-			  //if the value is greater than the next two bottom values then u stop.
+	pushDownHelp(1);
     }
+
+	private void pushDownHelp(int x){
+	if(x < arrayize() - 1){
+	String parent = array.get(x);
+	String childLeft = array.get(2 * x);
+	String childRight = array.get(2 * x + 1);
+	if(childLeft.compareTo(childRight) >= 1){
+	if(parent.compareTo(childLeft) >= 1){
+	array.set(2 * x) = parent;
+	array.set(x) = childLeft;
+	pushDownHelp(2 * x);
+	}
+}
+	else{
+	if(parent.compareTo(childRight) >= 1){
+	array.set(2 * x + 1) = parent;
+	array.set(x) = childRight;
+	pushDownHelp(2 * x + 1);
+	}
+}
+}
+
+//Use a while loop instead of helper fuctions to make the pushUp and pushDown functions.
