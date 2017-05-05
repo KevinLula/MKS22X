d8b5e02d1598d4ecbd0c74c9ef9d4cb3bcfacbaf
KevinLula <kevinlula2014@gmail.com>
2017-05-04 22:57:34 -0400

    Working with array list but might make it into an array

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,33 +1,41 @@
 import java.util.ArrayList;
 public class MyHeap{
 
-    private int constant;
+    private int direction;
+    private int size;
     private ArrayList<String> array;
 
     public MyHeap(){
-	constant = 1;
-	array.add("null");
+	
+	direction = 1;
+	array.add("");
+	size = 0;
     }
 
     public MyHeap(boolean x){
 	if(x){
-	    array.add("null");
-	    constant = 1;
+	    array.add("");
+	    direction = 1;
+	    size = 0;
 	}
 	else{
 	    array.add("null");
-	    constant = -1;
+	    direction = -1;
+	    size = 0;
 	}
     }
 
     public void add(String s){
+	size += 1;
 	array.add(s);
 	pushUp();
     }
 
     public String remove(){
-	return array.remove();
+        String res = array.get(1);
+	array.set(1, array.get(size));
 	pushDown();
+	return res;
     }
 
     public String peek(){
@@ -42,8 +50,8 @@ private void pushUpHelp(int x){
 	String str = array.get(x);
 	if(x > 1){
 	if(str.compareTo(array.get(x/2)) >= 1){
-	array.set(x) = array.get(x/2);
-	array.set(x/2) = str;
+	    array.set(x,array.get(x/2));
+	    array.set(x/2, str);
 	pushUpHelp(x/2);
 	}
 }
@@ -60,20 +68,37 @@ private void pushUpHelp(int x){
 	String childRight = array.get(2 * x + 1);
 	if(childLeft.compareTo(childRight) >= 1){
 	if(parent.compareTo(childLeft) >= 1){
-	array.set(2 * x) = parent;
-	array.set(x) = childLeft;
+	    array.set(2 * x,parent);
+	    array.set(x, childLeft);
 	pushDownHelp(2 * x);
 	}
 }
 	else{
 	if(parent.compareTo(childRight) >= 1){
-	array.set(2 * x + 1) = parent;
-	array.set(x) = childRight;
+	    array.set(2 * x + 1, parent);
+	    array.set(x, childRight);
 	pushDownHelp(2 * x + 1);
 	}
 }
 }
 }
+
+    public String toString(){
+	String res = "[";
+	for(int i = 1; i <= size; i++){
+	    res += array.get(i) + " ";
+	}
+	res += "]";
+	return res;
+    }
+	
+    public static void main(String[] args){
+	MyHeap x = new MyHeap();
+	x.add("a");
+	x.add("b");
+	x.add("c");
+	System.out.println(x);
+    }
 }
 
 //Use a while loop instead of helper fuctions to make the pushUp and pushDown functions.
