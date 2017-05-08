313155acaebb3be3288c76dbf5b71a0c27ad1373
Kevin <kevinlula2014@gmail.com>
2017-05-08 15:25:19 -0400

    Started

---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,13 @@
+public class RunningMedian{
+
+    public RunningMedian(){
+    }
+    //Make an empty RunningMedian
+
+    public void add(int){
+    }
+    //insert the value into the heap
+
+    public double getMedian(){
+    }
+    //return the median (DO NOT REMOVE ANYTHNG)
